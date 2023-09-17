6204c1e11b37dd513d6f00750229dc2909152c09
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2023-09-17 23:39:13 +0300

    update and add parallax project

---
+++ parallax/parallax/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import "./style.css";
+import index from "./index"
+function App() {
+  return (
+    <div className="App">
+      {<index/>}
+    </div>
+  );
+}
+
+export default App;
